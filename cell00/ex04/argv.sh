e604e094168bdc4e5856f649cfb28f6de351525a
psritham <not852298@gmail.com>
2026-09-18 02:05:34 +0700

    add new cell00 ex04

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,13 +1,7 @@
 if [ $# -eq 0 ]; then
     echo "No arguments supplied"
 else
-    if [ $# -ge 1 ]; then
-        echo "$1"
-    fi
-    if [ $# -ge 2 ]; then
-        echo "$2"
-    fi
-    if [ $# -ge 3 ]; then
-        echo "$3"
-    fi
+    for arg in "$@"; do
+        echo "$arg"
+    done
 fi
